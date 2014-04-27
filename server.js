21c7828418dc755af7c0702e4c73a10017524f17
Rudolf Hersen <rhersen@gmail.com>
2014-04-27 22:10:21 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 var express = require('express');
 var request = require('request');
-var key = require('./key');
 var parser = require('./parser');
 
 var app = express();
 
-app.get('/departures/:id', function (req, res) {
+app.get('/tweets', function (req, res) {
     request({
-            uri: 'https://api.trafiklab.se/sl/realtid/GetDpsDepartures?key=' + key.getKey() + '&timeWindow=60&siteId=' + req.params.id,
+            uri: 'https://api.twitter.com/1.1/statuses/user_timeline.json' +
+               '?user_id=1841700720' +
+               '&exclude_replies=true' +
+               '&include_rts=false' +
+               '&count=200' +
+               '&since_id=446228899090137087' +
+               '',
             headers: {
-                accept: 'application/json'
+                authorization: "Bearer " + "AAAAAAAAAAAAAAAAAAAAACMAXQAAAAAAYw3Z1oZKNFz52BtTRYdtkfa8Iwc%3DRJihktYblAngUJ9jMdbFBemI1PQO9eDNxbK7tANe8w3r36jH1D"
             }},
         function (error, response, body) {
             res.setHeader('Content-Type', 'application/json');
-            res.send(parser.getTrains(body));
+            res.send(body);
         });
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
-console.log('Listening on port 3000');
+var port = 3747;
+app.listen(port);
+console.log('Listening on port ' + port);
