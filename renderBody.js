2c96da16949cf3bd63d2ec9a18a4356f5244fab8
Rudolf Hersén <rudolf.hersen@schibsted.se>
2018-01-23 22:37:33 +0100

    Refactor renderTweet

--- renderBody.js
+++ renderBody.js
@@ -5,40 +5,40 @@ module.exports = function(tweets) {
 };
 
 function renderTweet(d) {
-  const time = d.created_at ? d.created_at.substr(8, 8) : 'When?';
-  const user =
-    d.retweeted_status && d.retweeted_status.user
-      ? d.retweeted_status.user.screen_name
-      : d.user ? d.user.screen_name : 'Who?';
-  const retweeter =
-    d.retweeted_status && d.user && d.user.screen_name
-      ? ` <i>${d.user.screen_name}</i> `
-      : ' ';
-  const text = d.retweeted_status ? d.retweeted_status.full_text : d.full_text;
+  const rs = d.retweeted_status;
   let image = '';
+
+  addImages(d);
+
+  if (rs) addImages(rs);
+
+  const time = d.created_at ? d.created_at.substr(8, 8) : 'When?';
+  const data = JSON.stringify(d).replace(/'/g, '');
+  const user =
+    rs && rs.user ? rs.user.screen_name : d.user ? d.user.screen_name : 'Who?';
+  const a = `<a href='/mark?id=${d.id_str}'>${time}</a>`;
+  const i =
+    rs && d.user && d.user.screen_name ? ` <i>${d.user.screen_name}</i> ` : ' ';
+  const b = `<b onclick='const data = ${data};console.log(data)'>${user}</b>`;
+  const text = rs ? rs.full_text : d.full_text;
+
+  return `<li>${a}${i}${b} ${text} ${image}</li>`;
+
+  function addImages(d) {
+    if (d.entities && d.entities.media) {
+      d.entities.media.filter(isPhoto).forEach(addImage);
+    }
+  }
 
   function addImage(img) {
     const size = img.sizes.small;
-    image += `<img src="${img.media_url}:small" width="${size.w /
-      2}" height="${img.sizes.small.h / 2}" />`;
+    const width = size.w / 2;
+    const height = size.h / 2;
+    const src = `${img.media_url}:small`;
+    image += `<img src="${src}" width="${width}" height="${height}" />`;
   }
+}
 
-  if (d.entities && d.entities.media) {
-    d.entities.media.filter(img => img.type === 'photo').forEach(addImage);
-  }
-
-  if (
-    d.retweeted_status &&
-    d.retweeted_status.entities &&
-    d.retweeted_status.entities.media
-  ) {
-    d.retweeted_status.entities.media
-      .filter(img => img.type === 'photo')
-      .forEach(addImage);
-  }
-
-  const data = JSON.stringify(d).replace(/'/g, '');
-  return `<li><a href='/mark?id=${
-    d.id_str
-  }'>${time}</a>${retweeter}<b onclick='const data = ${data};console.log(data)'>${user}</b> ${text} ${image}</li>`;
+function isPhoto(img) {
+  return img.type === 'photo';
 }
